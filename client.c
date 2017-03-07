bd71c517482f716fe51e7af16535386b8c94ed07
Oana <oana_gugea@yahoo.com>
2017-03-07 11:31:39 +0200

    Update client.c
    +comentarii

--- client.c
+++ client.c
@@ -6,31 +6,34 @@
 #include <unistd.h>
 
 int main(){
-  int clientSocket;
-  char buffer[1024], buff[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
+	int clientSocket;
+	char buffer[1024], buff[1024];
+	struct sockaddr_in serverAddr;
+	socklen_t addr_size;
 
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  serverAddr.sin_family = AF_INET;
- 
-  serverAddr.sin_port = htons(7891);
+	//socket()
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	//configurare structura pentru adresa serverului
+	//familia de protocoale TCP/IP
+	serverAddr.sin_family = AF_INET;
+	//numar port
+	serverAddr.sin_port = htons(7891);
+	//adresa IP (local host)
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+	//connect()
+	connect(clientSocket, (struct sockaddr *) &serverAddr, (sizeof serverAddr));
 
-  while(1){
-	  scanf("%s", buffer);
-	  send(clientSocket,buffer, 10,0);
-	  strcpy(buffer,"");
-	  recv(clientSocket, buffer, 10, 0);
-	  printf("Data received from server: %s\n",buffer);
-	  strcpy(buffer,"");
-  }
-  return 0;
+	while(1){
+		printf("Client: ");
+		scanf("%s", buffer);
+		send(clientSocket,buffer, 10,0);
+		strcpy(buffer,"");
+		recv(clientSocket, buffer, 10, 0);
+		printf("Server: %s\n",buffer);
+		strcpy(buffer,"");
+		printf("\n");
+	}
+	return 0;
 }
